380049553add9392c8e6042ffd497224e0c92c53
dragon <isanoguchi721@gmail.com>
2022-11-30 07:22:44 -0800

    fix

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,5 @@
 import Logo from "assets/svg/logo.svg";
-import Account from "assets/img/account.png";
 import Connect from "assets/img/connect.png";
-import Man from "assets/img/man.png";
 import Tab from "components/tab";
 
 import { InjectedConnector } from "@web3-react/injected-connector";
@@ -22,7 +20,6 @@ const Header = () => {
         <Tab />
       </div>
       <div className="my-auto flex">
-        <img src={Account} alt="account" className="w-[40px]" />
         <div className="my-auto ml-[10px] mr-[20px] h-[40px]">
           {!account ? (
             <button
@@ -48,11 +45,6 @@ const Header = () => {
             </button>
           )}
         </div>
-        <img
-          className="w-[40px] h-[40px] rounded-[50%]"
-          src={Man}
-          alt="account"
-        />
       </div>
     </div>
   );
